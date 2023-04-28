53e5c0ad04139c92eec3533cd23043737c875a74
Achraf Bardan <abardan@fletcher.nl>
2023-04-28 10:46:48 +0200

    feat: :fire: Set default resource resolver in service provider.

--- src/SimpleResourcesServiceProvider.php
+++ src/SimpleResourcesServiceProvider.php
@@ -13,4 +13,24 @@ class SimpleResourcesServiceProvider extends PackageServiceProvider
             ->name('simple-resources')
             ->hasConfigFile();
     }
+
+    /**
+     * Bootstrap any package services.
+     */
+    public function boot(): void
+    {
+        ResourceFactory::guessResourceNameUsing(function (string $modelName) {
+            $guess = sprintf(
+                '%s\\%sResource',
+                config('simple-resources.resource_namespace'),
+                str_replace(config('simple-resources.model_namespace').'\\', '', $modelName),
+            );
+
+            if (class_exists($guess)) {
+                return $guess;
+            }
+
+            return ResourceFactory::$defaultResourceClass;
+        });
+    }
 }
